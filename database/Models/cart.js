3f68377638bcce68d61cea9d2675883a1b9db0f0
LMY03 <lj021803@gmail.com>
2022-11-16 19:42:45 +0800

    Add files via upload

---
+++ database/Models/cart.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const cartSchema = new mongoose.Schema({
+    username: {
+        type: String, required: true, immutable: true
+    },
+    artNames: {
+        type: Array, required: true, immutable: true
+    },
+    artDetails: {
+        type: Array, required: true, immutable: true
+    }
+});
+
+module.exports = mongoose.model('cart', cartSchema);
